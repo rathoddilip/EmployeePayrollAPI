63372ce00bb1a9708b095985dcbf427b9d057496
diliprathod32 <diliprathod32@gmail.com>
2021-03-23 15:25:01 +0530

    [Dilip] Modify:EmployeePayrollAPI

--- StoreProcedure/dbo.sp_AddEmployeeRecord.sql
+++ StoreProcedure/dbo.sp_AddEmployeeRecord.sql
@@ -1,30 +1,46 @@
 ﻿CREATE PROCEDURE [dbo].[sp_AddEmployeeRecord]
 	-- Add the parameters for the stored procedure here
-	@Name			varchar(255),
-	@Gender			varchar(10),
-	@Salary			bigint,
-	@StartDate		Date
+	@Name				varchar(255),
+	@Gender				varchar(10),
+	@Salary				bigint,
+	@StartDate			Date,
+	@ProfileImage   	varchar(50),
+	@Notes				varchar(250),
+	@Department1		varchar(50),
+	@Department2		varchar(50),
+	@Department3		varchar(50)
 AS
-begin transaction
-begin try
-   INSERT INTO Employee(Name, Gender, Salary,StartDate)
-	VALUES(@Name, @Gender, @Salary, @StartDate );
-end try
-begin catch
-   SELECT
-        ERROR_NUMBER() AS ErrorNumber,
-        ERROR_SEVERITY() AS ErrorSeverity,
-        ERROR_STATE() AS ErrorState,
-        ERROR_PROCEDURE() AS ErrorProcedure,
-        ERROR_LINE() AS ErrorLine,
-        ERROR_MESSAGE() AS ErrorMessage;
-   while(@@trancount > 0)
-   begin
-      rollback transaction
-   end
-end catch
-if (@@trancount <> 0)
-begin
-   commit transaction;
-end
+BEGIN
+SET NOCOUNT ON
+IF EXISTS (SELECT * FROM [Employee] WHERE Name = @Name)
 
+BEGIN
+
+RAISERROR ('Record Already Exist',
+
+16, -- Severity,
+
+1 -- State,
+
+)
+
+RETURN -1
+
+END
+
+else
+
+Begin
+DECLARE @EmployeeId int;
+ SET NOCOUNT ON;
+   INSERT INTO Employee(Name, Gender, Salary, StartDate,ProfileImage,Notes)
+	VALUES(@Name, @Gender, @Salary, @StartDate,@ProfileImage, @Notes); 
+SET @EmployeeId = SCOPE_IDENTITY();
+INSERT INTO Department(DeptID, Department1,Department2,Department3)
+Values(@EmployeeId,@Department1,@Department2,@Department3)
+
+--select Employee.ID,Employee.Name,Employee.Gender,Employee.Salary,Employee.StartDate,Employee.ProfileImage,Employee.Notes, Department.Department1, Department.Department2, Department.Department3 from Employee
+--inner join Department 
+--on Employee.ID=Department.DeptID;
+END
+end
--- StoreProcedure/dbo.sp_GetEmployeePayrollAllData.sql
+++ StoreProcedure/dbo.sp_GetEmployeePayrollAllData.sql
@@ -2,5 +2,8 @@
 CREATE PROCEDURE [dbo].[sp_GetEmployeePayrollAllData] 
 
 AS BEGIN
-SELECT * FROM Employee
+--SELECT * FROM Employee;
+select Employee.ID,Employee.Name,Employee.Gender,Employee.Salary,Employee.StartDate,Employee.ProfileImage,Employee.Notes, Department.DeptID, Department.Department1, Department.Department2, Department.Department3 from Employee
+inner join Department 
+on Employee.ID=Department.DeptID;
 END;
